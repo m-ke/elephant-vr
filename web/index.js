5079db1d777544b2941652147c2aaf388cb6d304
Martin Splitt <mr.avgp@googlemail.com>
2017-12-15 15:02:05 +0100

    Cleaning up the style

--- web/index.js
+++ web/index.js
@@ -1,81 +1,68 @@
+const scene = document.querySelector('#scene');
+const elephant = document.querySelector('#elephant');
+const remote = document.querySelector('#remote');
+const wobbleAnimation = document.querySelector('#wobble');
+
+const cactusIds = ['70369520-e9bb-4895-9d7e-c32a64df3db3', 'f8b69c6b-74f0-40f9-916f-c5293f27fe10'];
+const cactusKinds = 2;
+const cactusCount = 12;
+
+const distance = 40;
+const jumpAccelerationTreshold = 20;
+let jumping = false;
+
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     console.log('loaded')
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
-const scene = document.querySelector('#scene');
-const elephant = document.querySelector('#elephant');
-const remote = document.querySelector('#remote');
-const wobbleAnimation = document.querySelector('#t-rex-wobble');
 
-const mode = '3dio';
-const cactusIds = ['70369520-e9bb-4895-9d7e-c32a64df3db3', 'f8b69c6b-74f0-40f9-916f-c5293f27fe10'];
-const cactusKinds = 7;
-
-const distance = 40;
-const cactusCount = 12;
-const jumpAccelerationTreshold = 20;
-
-
-function addCacti() {
-  // create cactus entitites and generate random positions and rotations
-  for (i = 0; i < cactusCount; i++) {
-    let entity = document.createElement('a-entity');
-    let angle = 360. / cactusCount * i;
-    if (Math.random() > 0.5) {
-      angle -= 360. / cactusCount + 8;
-    }
-    entity.setAttribute('position', {
-      x: distance * Math.cos(angle / 180 * Math.PI),
-      y: 0,
-      z: -distance * Math.sin(angle / 180 * Math.PI),
-    });
-    entity.setAttribute('rotation', {
-      x: 0,
-      y: angle + 90,
-      z: 0,
-    });
-
-    const cactusIndex = Math.ceil(cactusKinds * Math.random());
-
-    // use basic black cactus models
-    if (mode === 'basic') {
-      entity.setAttribute('obj-model', {
-        obj: `assets/cactus-${cactusIndex}.obj`
-      });
-      entity.setAttribute('scale', { x: 10, y: 10, z: 10 });
-      entity.setAttribute('material', { color: 'black' });
-    } else {
-      entity.setAttribute('io3d-furniture', `id: ${cactusIds[cactusIndex % 2]}`)
-      entity.setAttribute('scale', { x: 9, y: 9, z: 9 });
-    }
-    // add cactus to the scene
-    scene.appendChild(entity);
+// create cactus entitites and generate random positions and rotations
+for (i = 0; i < cactusCount; i++) {
+  let entity = document.createElement('a-entity');
+  let angle = 360. / cactusCount * i;
+  if (Math.random() > 0.5) {
+    angle -= 360. / cactusCount + 8;
   }
+  entity.setAttribute('position', {
+    x: distance * Math.cos(angle / 180 * Math.PI),
+    y: 0,
+    z: -distance * Math.sin(angle / 180 * Math.PI),
+  });
+  entity.setAttribute('rotation', {
+    x: 0,
+    y: angle + 90,
+    z: 0,
+  });
+
+  let cactusIndex = Math.floor(cactusKinds * Math.random());
+  entity.setAttribute('io3d-furniture', `id: ${cactusIds[cactusIndex]}`)
+  entity.setAttribute('scale', { x: 9, y: 9, z: 9 });
+  // add cactus to the scene
+  scene.appendChild(entity);
 }
 
 function animateJump() {
   elephant.removeChild(wobbleAnimation);
+
   elephant.setAttribute('rotation', { x: 0, y: 90, z: 0 });
   return new Promise(resolve => {
     const jumpAnimation = document.querySelector('#jumpAnimation');
     const animationFragment = document.importNode(jumpAnimation.content, true);
-    animationFragment.firstElementChild.addEventListener('animationend', () => {
+    animationFragment.querySelector('.down').addEventListener('animationend', () => {
       elephant.appendChild(wobbleAnimation);
       resolve();
     });
     elephant.appendChild(animationFragment);
   });
 }
-
-let jumping = false;
 
 function jump() {
   if (!jumping) {
@@ -99,4 +86,4 @@ window.addEventListener('devicemotion', (e) => {
 }, true);
 
 remote.addEventListener('buttondown', jump);
- 
+
